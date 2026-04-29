e809f4df2924eae21d21f513118e0bb16a1c48ce
Bao Doan <78118831+baomdoan@users.noreply.github.com>
2026-04-29 12:06:38 -0400

    Completed ex 1-7

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let message = '';
 
+    if (num < 0)
+    {
+        message = 'ERROR';
+    }
+    else
+    {
+        for (let i = 0; i < num; i++)
+        {
+            message += str;
+        }
+    }
+
+    return message;
 };
 
 // Do not edit below this line
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let revstring = '';
+    const length = str.length;
 
+    for (let i = length - 1; i >= 0; i--)
+    {
+        revstring += str[i];
+    }
+
+    return revstring;
 };
 
 // Do not edit below this line
