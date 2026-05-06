cd5522584b85143e0545363f3e2c1347b3b07c8e
Bao Doan <78118831+baomdoan@users.noreply.github.com>
2026-05-06 16:11:28 -0400

    Completed exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num) {
+    let newArray = [];
+    const arrayLength = array.length;
+
+    for (let i = 0; i < arrayLength; i++)
+    {
+        if (array[i] != num)
+        {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
