4c72d46f7927e7c33ba95a724f3b6b7098799d4a
Bao Doan <78118831+baomdoan@users.noreply.github.com>
2026-05-27 15:03:52 -0400

    Completed exercise 9-11

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < num2)
+    {
+        for (let i = num1; i <= num2; i++)
+        {
+            sum += i;
+        }
+    }
+    else
+    {
+        for (let i = num1; i >= num2; i--)
+        {
+            sum += i;
+        }
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const temp = Math.round((tempF - 32) / (9/5));
+
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  const temp = Math.round(tempC * (9/5) + 32);
+
+  return temp;
 };
 
 // Do not edit below this line
